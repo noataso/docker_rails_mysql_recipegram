cb2ee6792118729ea279a53ca6a23d76b855c8c0
noataso <noa740740@outlook.jp>
2023-04-15 15:43:04 +0900

    変更

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '4fba35f8fc7e941b705a0962cceade09fad86778eb3c00ccff91f482e2db7d1f67716e4f6e6b22a5f85ae395eb3c821ad4b6c7bca757dd9606835b883c0819fa'
